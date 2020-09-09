61e826ff33dd9cec0b67e8af92cc80b78426de27
yaoqingjia <yaoqingjia@orientsec.com.cn>
2020-09-09 15:31:18 +0800

    dd

--- src/main/java/com/jenkins/jenkins_github/JenkinsGithubApplication.java
+++ src/main/java/com/jenkins/jenkins_github/JenkinsGithubApplication.java
@@ -8,6 +8,7 @@ public class JenkinsGithubApplication {
 
     public static void main(String[] args) {
         System.out.println("启动");
+
         SpringApplication.run(JenkinsGithubApplication.class, args);
     }
 
