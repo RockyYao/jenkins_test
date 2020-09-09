b15db21f946914a23f040045e1fac8cc382b78f3
yaoqingjia <yaoqingjia@orientsec.com.cn>
2020-09-09 16:00:54 +0800

    单元测试

--- src/test/java/com/jenkins/jenkins_github/JenkinsGithubApplicationTests.java
+++ src/test/java/com/jenkins/jenkins_github/JenkinsGithubApplicationTests.java
@@ -1,6 +1,6 @@
 package com.jenkins.jenkins_github;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +8,7 @@ class JenkinsGithubApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("执行单元测试");
     }
 
 }
